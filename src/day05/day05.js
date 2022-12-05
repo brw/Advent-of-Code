115a588b8915012a21a5928e27409136900736dd
Bas van den Wollenberg <hi@bas.sh>
2022-12-05 08:49:59 +0100

    Reverse stacks and finish part 2

--- src/day05/day05.js
+++ src/day05/day05.js
@@ -2,50 +2,65 @@ import { readFileSync } from 'node:fs';
 
 const input = readFileSync('./input.txt', 'utf-8');
 
-const [drawing, instructions] = input.split('\n\n');
+function parseInput(input) {
+  let [stacks, moves] = input.split('\n\n');
 
-const moves = instructions
-  .trim()
-  .split('\n')
-  .reduce(
-    (moves, instruction) =>
-      moves.concat([
-        instruction.match(/move (?<amount>\d+) from (?<from>\d+) to (?<to>\d+)/)
-          .groups,
-      ]),
-    []
-  );
+  moves = moves
+    .trim()
+    .split('\n')
+    .reduce((moves, move) => moves.concat([parseMove(move)]), []);
 
-// const amount = Number([...drawing.matchAll(/\d+/g)].at(-1)[0]);
-// const initialState = Array(amount).fill([]);
-
-const stacks = drawing
-  .split('\n')
-  .slice(0, -1)
-  .reduce((stacks, line, index) => {
-    for (let i = 0; i < line.length; i++) {
-      if ((i - 1) % 4 == 0) {
-        const stack = (i - 1) / 4;
-        if (line[i] !== ' ') {
-          if (!stacks[stack]) {
-            stacks[stack] = [line[i]];
-          } else {
-            stacks[stack].push(line[i]);
+  stacks = stacks
+    .split('\n')
+    .slice(0, -1)
+    .reduce((stacks, line) => {
+      for (let i = 0; i < line.length; i++) {
+        if ((i - 1) % 4 == 0) {
+          const idx = (i - 1) / 4;
+          if (line[i] !== ' ') {
+            if (!stacks[idx]) {
+              stacks[idx] = [line[i]];
+            } else {
+              stacks[idx].unshift(line[i]);
+            }
           }
         }
       }
-    }
-    return stacks;
-  }, []);
-
-console.log(stacks);
-
-for (const move of moves) {
-  const from = move.from - 1;
-  const to = move.to - 1;
-  for (let i = 0; i < move.amount; i++) {
-    stacks[to].unshift(stacks[from].shift());
-  }
+      return stacks;
+    }, []);
+  return { moves, stacks };
 }
 
-console.log(stacks.map((stack) => stack[0]).join(''));
+function parseMove(move) {
+  return move.match(/move (?<amount>\d+) from (?<from>\d+) to (?<to>\d+)/)
+    .groups;
+}
+
+function part1(input) {
+  const { moves, stacks } = parseInput(input);
+
+  for (const move of moves) {
+    const from = move.from - 1;
+    const to = move.to - 1;
+    for (let i = 0; i < move.amount; i++) {
+      stacks[to].push(stacks[from].pop());
+    }
+  }
+
+  return stacks.map((stack) => stack.at(-1)).join('');
+}
+
+function part2(input) {
+  const { moves, stacks } = parseInput(input);
+
+  for (const move of moves) {
+    const from = move.from - 1;
+    const to = move.to - 1;
+    stacks[to].push(...stacks[from].slice(-move.amount));
+    stacks[from] = stacks[from].slice(0, stacks[from].length - move.amount);
+  }
+
+  return stacks.map((stack) => stack.at(-1)).join('');
+}
+
+console.log(part1(input), part2(input));
