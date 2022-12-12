dcd24b09fc4f612591fced319eaa8d90bd8c3061
Bas van den Wollenberg <hi@bas.sh>
2022-12-12 22:52:01 +0100

    Add some helper functions and keep track of costs for each node instead of backtracking at the end

--- src/day12/day12.js
+++ src/day12/day12.js
@@ -7,63 +7,65 @@ function parseHeightMap(data) {
   let end = [];
   const possibleStart = [];
   const lines = data.trim().split('\n');
-  const map = lines.map((line, i) =>
-    line.split('').map((char, j) => {
+  const map = lines.map((line, y) =>
+    line.split('').map((char, x) => {
       if (char === 'S') {
-        start = [j, i];
+        start = [x, y];
         possibleStart.push(start);
-        return 'a'.charCodeAt(0);
+        char = 'a';
       } else if (char === 'E') {
-        end = [j, i];
-        return 'z'.charCodeAt(0);
+        end = [x, y];
+        char = 'z';
       } else if (char === 'a') {
-        possibleStart.push([j, i]);
-        return 'a'.charCodeAt(0);
-      } else {
-        return char.charCodeAt(0);
+        possibleStart.push([x, y]);
+        char = 'a';
       }
+      return char.charCodeAt(0);
     })
   );
-
   return { map, start, end, possibleStart };
 }
 
-function boundsCheck(node, map) {
-  const [x, y] = node;
-  return x >= 0 && y >= 0 && y < map.length && x < map[y].length;
-}
+const boundsCheck = ([x, y], map) =>
+  x >= 0 && y >= 0 && y < map.length && x < map[y].length;
+
+const add = ([x1, y1], [x2, y2]) => [x1 + x2, y1 + y2];
+
+const compare = ([x1, y1], [x2, y2]) => x1 === x2 && y1 === y2;
+
+const str = (node) => `${node[0]},${node[1]}`;
+
+const getHeight = ([x, y], map) => map[y][x];
 
 function findShortestDistance(start, end, map) {
-  const moves = [
+  const MOVES = [
     [0, 1],
     [0, -1],
     [1, 0],
     [-1, 0],
   ];
-  const visited = new Set([start.join()]);
+  const visited = new Set([str(start)]);
   const queue = [start];
-  const prev = {};
+  const cost = { [str(start)]: 0 };
 
   while (queue.length > 0) {
     let node = queue.shift();
-    if (node[0] === end[0] && node[1] === end[1]) {
-      let steps = 0;
-      while (!(node[0] === start[0] && node[1] === start[1])) {
-        node = prev[node.join()];
-        steps++;
-      }
-      return steps;
+
+    if (compare(node, end)) {
+      return cost[str(node)];
     }
-    for (const move of moves) {
-      const newNode = [node[0] + move[0], node[1] + move[1]];
+
+    for (const move of MOVES) {
+      const newNode = add(node, move);
+
       if (
         boundsCheck(newNode, map) &&
-        !visited.has(newNode.join()) &&
-        map[newNode[1]][newNode[0]] - map[node[1]][node[0]] <= 1
+        !visited.has(str(newNode)) &&
+        getHeight(newNode, map) - getHeight(node, map) <= 1
       ) {
         queue.push(newNode);
-        visited.add(newNode.join());
-        prev[newNode.join()] = node;
+        visited.add(str(newNode));
+        cost[str(newNode)] = cost[str(node)] + 1;
       }
     }
   }
@@ -77,12 +79,14 @@ export function part1(data) {
 export function part2(data) {
   const { map, possibleStart, end } = parseHeightMap(data);
   let minSteps;
+
   for (const start of possibleStart) {
     const newSteps = findShortestDistance(start, end, map);
     if (!minSteps || newSteps < minSteps) {
       minSteps = newSteps;
     }
   }
+
   return minSteps;
 }
 
