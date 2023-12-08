59e3dd0e8b9ac1f66c684148e3ec24795390b63c
Bas van den Wollenberg <hi@bas.sh>
2023-12-08 17:49:14 +0100

    Actually use the example given for part 2 as a test
    I didn't realize I forgot to use it lmao

--- src/2023/day08/index.test.ts
+++ src/2023/day08/index.test.ts
@@ -1,16 +1,27 @@
 import { expect, test } from 'bun:test';
 import { part1, part2 } from './index.js';
 
-const input = `LLR
+const input1 = `LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`;
 
+const input2 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
 test('part1', () => {
-  expect(part1(input)).toEqual(6);
+  expect(part1(input1)).toEqual(6);
 });
 
 test('part2', () => {
-  expect(part2(input)).toEqual(6);
+  expect(part2(input2)).toEqual(6);
 });
