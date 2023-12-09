7ed2bcf8914c857b5aefb8209f75b584a129b3c0
Bas van den Wollenberg <hi@bas.sh>
2023-12-09 06:55:44 +0100

    Clean up

--- src/2023/day09/index.ts
+++ src/2023/day09/index.ts
@@ -3,30 +3,9 @@ export function part1(data: string) {
   const next = [];
 
   for (const line of lines) {
-    let values = line.split(' ').map((v) => Number(v));
-    const layers = [values];
-    while (true) {
-      const differences = values.reduce((acc, curr, i, arr) => {
-        if (i === 0) {
-          return acc;
-        }
+    const layers = createDifferenceLayer(line);
 
-        const difference = curr - arr[i - 1];
-        acc.push(difference);
-        return acc;
-      }, [] as number[]);
-      layers.push(differences);
-      if (differences.every((d) => d === 0)) {
-        break;
-      }
-      values = differences;
-    }
-
-    for (let i = layers.length - 1; i >= 0; i--) {
-      if (i === layers.length - 1) {
-        continue;
-      }
-
+    for (let i = layers.length - 2; i >= 0; i--) {
       const layer = layers[i];
       const prevLayer = layers[i + 1];
       layer.push(layer.at(-1)! + prevLayer.at(-1)!);
@@ -37,43 +16,14 @@ export function part1(data: string) {
   return next.reduce((a, b) => a + b);
 }
 
-function printLayers(layers: number[][]) {
-  for (let i = 0; i < layers.length; i++) {
-    const layer = layers[i];
-    console.log(' '.repeat(i) + layer.join(' '));
-  }
-  console.log();
-}
-
 export function part2(data: string) {
   const lines = data.trim().split('\n');
   const next = [];
 
   for (const line of lines) {
-    let values = line.split(' ').map((v) => Number(v));
-    const layers = [values];
-    while (true) {
-      const differences = values.reduce((acc, curr, i, arr) => {
-        if (i === 0) {
-          return acc;
-        }
+    const layers = createDifferenceLayer(line);
 
-        const difference = curr - arr[i - 1];
-        acc.push(difference);
-        return acc;
-      }, [] as number[]);
-      layers.push(differences);
-      if (differences.every((d) => d === 0)) {
-        break;
-      }
-      values = differences;
-    }
-
-    for (let i = layers.length - 1; i >= 0; i--) {
-      if (i === layers.length - 1) {
-        continue;
-      }
-
+    for (let i = layers.length - 2; i >= 0; i--) {
       const layer = layers[i];
       const prevLayer = layers[i + 1];
       layer.unshift(layer.at(0)! - prevLayer.at(0)!);
@@ -83,4 +33,34 @@ export function part2(data: string) {
   }
 
   return next.reduce((a, b) => a + b);
+}
+
+function createDifferenceLayer(line: string) {
+  let values = line.split(' ').map((v) => Number(v));
+  const layers = [values];
+
+  while (true) {
+    const differences = values.reduce((acc, curr, i, arr) => {
+      if (i === 0) return acc;
+
+      const difference = curr - arr[i - 1];
+      acc.push(difference);
+      return acc;
+    }, [] as number[]);
+    layers.push(differences);
+    if (differences.every((d) => d === 0)) {
+      break;
+    }
+    values = differences;
+  }
+
+  return layers;
+}
+
+function printLayers(layers: number[][]) {
+  for (let i = 0; i < layers.length; i++) {
+    const layer = layers[i];
+    console.log(' '.repeat(i) + layer.join(' '));
+  }
+  console.log();
 }
